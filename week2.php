0f60e9089e0f649cc2d7b280d614b164b3dcf5ea
Finayon <107600164+KokuFI@users.noreply.github.com>
2023-03-17 20:28:39 +0100

    Update week2.php

--- week2.php
+++ week2.php
@@ -4,11 +4,12 @@ $has_pvc = true;
 $ward = 021;
 
 if($has_pvc = false){
-    echo "Voter ineligible to vote because he/she does not have a PVC";
+    echo "Voter ineligible to vote because user does not have a PVC";
+
 }elseif($age < 18){
-    echo "Voter ineligible to vote because he/she is below 18 years of age";
+    echo "Voter ineligible to vote because user is below 18 years of age";
 }elseif($ward != 021 ){
-    echo "Voter ineligible to vote because his/her ward is not 021";
+    echo "Voter ineligible to vote because user ward is not 021";
 }else {
     echo "Voter eligible to vote.";
 }
